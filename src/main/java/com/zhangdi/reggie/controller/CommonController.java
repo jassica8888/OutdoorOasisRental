67ac6328743710d705b4e1ce34d5b01e4968266a
jassica8888 <zhangdi@graduate.utm.my>
2023-05-08 00:58:42 +0800

    Finish P53-File Download.

--- src/main/java/com/zhangdi/reggie/controller/CommonController.java
+++ src/main/java/com/zhangdi/reggie/controller/CommonController.java
@@ -3,12 +3,18 @@ package com.zhangdi.reggie.controller;
 import com.zhangdi.reggie.common.R;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.Servlet;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.UUID;
 
@@ -49,6 +55,33 @@ public class CommonController {
             e.printStackTrace();
         }
         return R.success(fileName);
+    }
+
+
+    @GetMapping("/download")
+    public void download(String name, HttpServletResponse response){
+
+        try {
+            //输入流，通过输入流读取文件内容
+            FileInputStream fileInputStream=new FileInputStream(new File(basePath+name));
+            //输出流，通过输出流将文件写回浏览器，在浏览器展示图片
+            ServletOutputStream outputStream=response.getOutputStream();
+
+            response.setContentType("image/jpeg");
+
+            int len = 0;
+            byte[] bytes=new byte[1024];
+            while((len=fileInputStream.read(bytes))!=-1){
+                outputStream.write(bytes,0,len);
+                outputStream.flush();
+            }
+            //关闭资源
+            outputStream.close();
+            fileInputStream.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 }
