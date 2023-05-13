b6bbc30cfd0fe8bf513c643ed5a84661822b10ce
jassica8888 <zhangdi@graduate.utm.my>
2023-05-14 00:21:46 +0800

    Finish P79-Package Deleted Successfully.

--- src/main/java/com/zhangdi/reggie/service/impl/SetmealServiceImpl.java
+++ src/main/java/com/zhangdi/reggie/service/impl/SetmealServiceImpl.java
@@ -1,6 +1,8 @@
 package com.zhangdi.reggie.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.zhangdi.reggie.common.CustomException;
 import com.zhangdi.reggie.dto.SetmealDto;
 import com.zhangdi.reggie.entity.Setmeal;
 import com.zhangdi.reggie.entity.SetmealDish;
@@ -36,5 +38,33 @@ public class SetmealServiceImpl extends ServiceImpl<SetmealMapper,Setmeal> imple
         }).collect(Collectors.toList());
         //保存套餐和菜品的关联信息，操作setmeal_dish，执行insert操作
         setmealDishService.saveBatch(setmealDishes);
+    }
+
+    /**
+     * 删除套餐，同时需要删除套餐和菜品的关联数据
+     * @param ids
+     */
+    @Transactional
+    public void removeWithDish(List<Long> ids){
+        //select count(*) from setmeal where id in (1,2,3) and status = 1
+        //查询套餐状态，确定是否可用删除
+        LambdaQueryWrapper<Setmeal> queryWrapper = new LambdaQueryWrapper();
+        queryWrapper.in(Setmeal::getId,ids);
+        queryWrapper.eq(Setmeal::getStatus,1);
+
+        int count = this.count(queryWrapper);
+        if(count > 0){
+            //如果不能删除，抛出一个业务异常
+            throw new CustomException("Package is in sell,deletion not allowed");
+        }
+
+        //如果可以删除，先删除套餐表中的数据---setmeal
+        this.removeByIds(ids);
+
+        //delete from setmeal_dish where setmeal_id in (1,2,3)
+        LambdaQueryWrapper<SetmealDish> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        lambdaQueryWrapper.in(SetmealDish::getSetmealId,ids);
+        //删除关系表中的数据----setmeal_dish
+        setmealDishService.remove(lambdaQueryWrapper);
     }
 }
